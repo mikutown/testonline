707baaa014c878eb932dfc4ff9bc517bed6d97be
mikutown <503944896@qq.com>
2020-04-10 15:04:55 +0800

    添加了数据表exam_ques 自动生成了Exam_ques的domain以及mapper

---
+++ src/main/java/top/leafii/testonline/common/domain/Exam_ques.java
@@ -0,0 +1,23 @@
+package top.leafii.testonline.common.domain;
+
+public class Exam_ques {
+    private Integer examId;
+
+    private Integer quesId;
+
+    public Integer getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Integer examId) {
+        this.examId = examId;
+    }
+
+    public Integer getQuesId() {
+        return quesId;
+    }
+
+    public void setQuesId(Integer quesId) {
+        this.quesId = quesId;
+    }
+}
---
+++ src/main/java/top/leafii/testonline/mapper/Exam_quesMapper.java
@@ -0,0 +1,30 @@
+package top.leafii.testonline.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.leafii.testonline.common.domain.Exam_ques;
+import top.leafii.testonline.common.domain.Exam_quesExample;
+
+public interface Exam_quesMapper {
+    long countByExample(Exam_quesExample example);
+
+    int deleteByExample(Exam_quesExample example);
+
+    int deleteByPrimaryKey(Integer examId);
+
+    int insert(Exam_ques record);
+
+    int insertSelective(Exam_ques record);
+
+    List<Exam_ques> selectByExample(Exam_quesExample example);
+
+    Exam_ques selectByPrimaryKey(Integer examId);
+
+    int updateByExampleSelective(@Param("record") Exam_ques record, @Param("example") Exam_quesExample example);
+
+    int updateByExample(@Param("record") Exam_ques record, @Param("example") Exam_quesExample example);
+
+    int updateByPrimaryKeySelective(Exam_ques record);
+
+    int updateByPrimaryKey(Exam_ques record);
+}
